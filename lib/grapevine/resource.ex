148371d48d24ec52207919cc37f7abb3d04462af
Adolfo Neto <adolfo.usp@gmail.com>
2021-07-01 20:26:32 -0300

    Add the migration and schema changes to implement the relationship between resources and users

--- lib/grapevine/resource.ex
+++ lib/grapevine/resource.ex
@@ -1,18 +1,19 @@
 defmodule Grapevine.Resource do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Grapevine.Accounts.User
 
   schema "resources" do
     field :content, :string
     field :title, :string
-
+    belongs_to :user, User
     timestamps()
   end
 
   @doc false
   def changeset(resource, attrs) do
     resource
-    |> cast(attrs, [:title, :content])
-    |> validate_required([:title, :content])
+    |> cast(attrs, [:title, :content, :user])
+    |> validate_required([:title, :content, :user])
   end
 end

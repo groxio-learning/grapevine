544e78a5dde88efeca05e711e9a6e1c479e9a5fb
Herminio Torres <herminiocesar@gmail.com>
2021-07-01 19:33:11 -0300

    apply mix format

--- lib/grapevine/accounts/user_token.ex
+++ lib/grapevine/accounts/user_token.ex
@@ -134,6 +134,7 @@ defmodule Grapevine.Accounts.UserToken do
   end
 
   def user_and_contexts_query(user, [_ | _] = contexts) do
-    from t in Grapevine.Accounts.UserToken, where: t.user_id == ^user.id and t.context in ^contexts
+    from t in Grapevine.Accounts.UserToken,
+      where: t.user_id == ^user.id and t.context in ^contexts
   end
 end
--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Grapevine.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false},
+      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false}
     ]
   end
 

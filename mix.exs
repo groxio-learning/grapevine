8cd21adfa93d9cf07006f50efe71661097bf6205
Cristine Guadelupe <cristineguadelupe@me.com>
2021-07-01 15:01:33 -0300

    Tailwind initial configuration

--- mix.exs
+++ mix.exs
@@ -33,15 +33,15 @@ defmodule Grapevine.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
+      {:phoenix, "~> 1.5.9"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.12.0"},
-      {:floki, ">= 0.0.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_view, "~> 0.15.7"},
+      {:floki, ">= 0.27.0", only: :test},
+      {:phoenix_html, "~> 2.14.3"},
+      {:phoenix_live_reload, "~> 1.3.2", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.3"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},

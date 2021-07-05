cd7e27e40a761882982ee402e54b3fc1500a54a7
Herminio Torres <herminiocesar@gmail.com>
2021-07-05 20:44:47 -0300

    teste

--- lib/grapevine/resource.ex
+++ lib/grapevine/resource.ex
@@ -4,16 +4,16 @@ defmodule Grapevine.Resource do
   alias Grapevine.Accounts.User
 
   schema "resources" do
-    field :content, :string
-    field :title, :string
-    belongs_to :user, User
-    timestamps()
+                field :content, :string
+                field :title, :string
+                belongs_to :user, User
+                timestamps()
   end
 
   @doc false
   def changeset(resource, attrs) do
-    resource
-    |> cast(attrs, [:title, :content, :user])
-    |> validate_required([:title, :content, :user])
+  resource
+  |> cast(attrs, [:title, :content, :user])
+  |> validate_required([:title, :content, :user])
   end
 end

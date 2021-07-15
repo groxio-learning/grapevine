99ca71b3431f46aaf5c6aa4ca46027f7a5f4b9e3
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2021-07-15 18:59:32 -0400

    revert removal of git commit hook

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule Grapevine.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
+      setup: ["deps.get", "ecto.setup", "git.hooks", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       "git.hooks": ["cmd cp .githooks/pre-commit .git/hooks/pre-commit"],

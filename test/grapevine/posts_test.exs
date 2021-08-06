7610f531aa48b004eabe9ec2e17c1b4cf8bdbd92
Jacqueline McKinney <jacqueline@truss.works>
2021-08-06 08:38:11 -0500

    formatter updates

--- test/grapevine/posts_test.exs
+++ test/grapevine/posts_test.exs
@@ -4,8 +4,6 @@ defmodule Grapevine.PostsTest do
   alias Grapevine.Post
   alias Grapevine.Posts
 
-
-
   describe "create/2" do
     test "successfully creates a new post" do
       %{id: id} = user_fixture()
@@ -22,11 +20,21 @@ defmodule Grapevine.PostsTest do
   end
 
   test "get all posts" do
-      %{id: id} = user_fixture()
-      {:ok, post_one} =  Posts.create(%{"title" => "my first post",  "content" => "https://google.com", "user_id" => id }, id)
-      {:ok, post_two} =  Posts.create(%{"title" => "the second one", "content" => "https://wikipedia.org", "user_id" => id}, id)
+    %{id: id} = user_fixture()
+
+    {:ok, post_one} =
+      Posts.create(
+        %{"title" => "my first post", "content" => "https://google.com", "user_id" => id},
+        id
+      )
+
+    {:ok, post_two} =
+      Posts.create(
+        %{"title" => "the second one", "content" => "https://wikipedia.org", "user_id" => id},
+        id
+      )
+
     test_posts = [post_one, post_two]
     assert test_posts == Posts.show_all()
   end
-
 end

dfee15082093b370d3b9028461dbe4f1660d3dc4
Herminio Torres <herminiocesar@gmail.com>
2021-09-23 00:10:59 -0300

    add an unlike handle_event

--- lib/grapevine_web/live/post_live/likes_component.ex
+++ lib/grapevine_web/live/post_live/likes_component.ex
@@ -1,24 +1,25 @@
 defmodule PostLive.LikesComponent do
-
-
   use GrapevineWeb, :live_component
 
   def update(assigns, socket) do
-    {:ok, socket |> assign(assigns) }
+    {:ok, socket |> assign(assigns)}
   end
 
-  def handle_event("like_it", _, %{assigns: %{post: post, current_user: user}} = socket) do
-    Grapevine.Posts.create_like(%{user_id: user.id, post_id: post.id})
+  def handle_event("like", _, %{assigns: %{post: post, current_user: user}} = socket) do
+    Grapevine.Posts.like(%{user_id: user.id, post_id: post.id})
     post = Grapevine.Posts.get(post.id)
-    {:noreply, 
-      socket
-      |> assign(%{post: post})
-    }
 
-
-
+    {:noreply,
+     socket
+     |> assign(%{post: post})}
   end
 
+  def handle_event("unlike", _, %{assigns: %{post: post, current_user: user}} = socket) do
+    Grapevine.Posts.unlike(%{user_id: user.id, post_id: post.id})
+    post = Grapevine.Posts.get(post.id)
 
-
+    {:noreply,
+     socket
+     |> assign(%{post: post})}
+  end
 end

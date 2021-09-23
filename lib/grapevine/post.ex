6b83c52f65e7c843e02e4ff043e753ad4e530177
tocteman <juanfrancisco.vinueza@gmail.com>
2021-09-23 18:33:44 -0500

    url validation

--- lib/grapevine/post.ex
+++ lib/grapevine/post.ex
@@ -18,6 +18,25 @@ defmodule Grapevine.Post do
     post
     |> cast(attrs, [:title, :content, :user_id])
     |> validate_required([:title, :content, :user_id])
+    |> validate_url()
     |> foreign_key_constraint(:user_id)
   end
+
+  def validate_url(changeset) do
+    validate_change(changeset, :content, fn :content, content ->
+      uri = URI.parse(content)
+      if uri.scheme != nil && uri.host =~ ~r/(www\.)?[[:alnum:]]\.[[:alnum:]]/i && uri.host =~ ~r/\A[^\.]/i do
+        []
+      else
+        [content: "Check the url." ]
+      end
+
+    end)
+
+
+  end
+
 end
+
+
+

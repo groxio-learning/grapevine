9bf70e93f0fde0f9b2903a4e27897423c7c73b16
Herminio Torres <herminiocesar@gmail.com>
2021-09-30 22:23:32 -0300

    add PhoenixPubSub to broadcast after create a new post

--- lib/grapevine_web/live/post_live/form_component.ex
+++ lib/grapevine_web/live/post_live/form_component.ex
@@ -18,7 +18,8 @@ defmodule PostLive.FormComponent do
   end
 
   def handle_event("save", %{"post" => post_params}, socket) do
-    Grapevine.Posts.create(post_params, socket.assigns.current_user.id)
+    post = Grapevine.Posts.create(post_params, socket.assigns.current_user.id)
+    Phoenix.PubSub.broadcast(Grapevine.PubSub, "posts", {:post_created, post})
     {:noreply, push_redirect(socket, to: "/posts")}
   end
 end
